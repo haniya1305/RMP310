a9d466d7c996286917f8c270807135a34d5a4ae8
D0ubleA-ron <adeo1@student.ubc.ca>
2022-10-18 00:40:37 -0700

    Completed CurrentReviews page

---
+++ app/src/main/java/com/example/ratemypritch/CurrentReviews.java
@@ -0,0 +1,14 @@
+package com.example.ratemypritch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentReviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_reviews);
+    }
+}
