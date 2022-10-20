bf0b69a9fb02c75a6a8ad4fd7f4a92a1a7efe57f
HANIYA <HANIYA@Haniyas-Mac.local>
2022-10-19 23:52:36 -0700

    created my reviews page

---
+++ app/src/main/java/com/example/ratemypritch/my_reviews.java
@@ -0,0 +1,14 @@
+package com.example.ratemypritch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_reviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_reviews);
+    }
+}
