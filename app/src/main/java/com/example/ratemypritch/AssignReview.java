aac47cc1dedb3a65fcb5ba065167158dafe0430a
D0ubleA-ron <adeo1@student.ubc.ca>
2022-10-21 12:07:26 -0700

    Finished all connections with other pages to Assign Review Page

--- app/src/main/java/com/example/ratemypritch/AssignReview.java
+++ app/src/main/java/com/example/ratemypritch/AssignReview.java
@@ -2,7 +2,14 @@ package com.example.ratemypritch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class AssignReview extends AppCompatActivity {
 
@@ -10,6 +17,55 @@ public class AssignReview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_review);
-        //Test Push
+        EditText commentstext = (EditText) findViewById(R.id.comments);
+        EditText commenthead = (EditText) findViewById(R.id.comments2);
+        RatingBar assignedrating = (RatingBar) findViewById(R.id.ratingBar);
+        ImageButton backbutton = (ImageButton) findViewById(R.id.imageButton6);
+        ImageButton assignreview = (ImageButton) findViewById(R.id.imageButton);
+
+        Intent intent = new Intent(this, CurrentReviews.class);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+        Intent intent2 = new Intent(this, my_reviews.class);
+        Bundle bundle = new Bundle();
+        assignreview.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(assignedrating.getRating() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "No Rating Inputted", Toast.LENGTH_SHORT).show();
+                }
+                else if ((commenthead.getText().toString().length() == 0 && !(commentstext.getText().toString().length() == 0) )|| (!(commenthead.getText().toString().length() == 0) && commentstext.getText().toString().length() == 0))
+                {
+                    Toast.makeText(getApplicationContext(), "Comment Inputted Incorrectly", Toast.LENGTH_SHORT).show();
+                }
+                else if (commenthead.getText().toString().length() == 0)
+                {
+                    bundle.putFloat("review", assignedrating.getRating());
+                    startActivity(intent2);
+                }
+                else
+                {
+                    bundle.putString("commenthead", commenthead.getText().toString());
+                    bundle.putString("commenthead", commentstext.getText().toString());
+                    bundle.putFloat("review", assignedrating.getRating());
+                    intent2.putExtras(bundle);
+                    startActivity(intent2);
+                }
+
+
+            }
+        });
+
+
+
+
+
     }
 }
