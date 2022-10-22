50e0ec8c7c4f8b40c8b78ecdabca8cdca4e77dd8
HANIYA <haniya1305@gmail.com>
2022-10-21 20:50:09 -0700

    my_review_page

--- app/src/main/java/com/example/ratemypritch/my_reviews.java
+++ app/src/main/java/com/example/ratemypritch/my_reviews.java
@@ -2,7 +2,11 @@ package com.example.ratemypritch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class my_reviews extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class my_reviews extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_reviews);
+        Button b1 = findViewById(R.id.button);
+        Button b2 = findViewById(R.id.button2);
+        ImageButton back_button = (ImageButton) findViewById(R.id.imagebutton);
+
+        Intent intent = new Intent(this, LandingPage.class);
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
+        Intent intent2 = new Intent(this, MainView.class);
+        back_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(intent2);
+            }
+        });
+
     }
 }
