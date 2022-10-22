d1be4d301851b9d580b9211f6a381d2a58e5cfe0
Jayati <jae186@gmail.com>
2022-10-21 19:41:04 -0700

    connected all

--- app/src/main/java/com/example/ratemypritch/MainView.java
+++ app/src/main/java/com/example/ratemypritch/MainView.java
@@ -2,13 +2,69 @@ package com.example.ratemypritch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageButton settingsButton, nourish, grill, chef, dessert;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
+        settingsButton = (ImageButton) findViewById(R.id.imageButton5);
+        nourish = (ImageButton) findViewById(R.id.imageButton2);
+        grill = (ImageButton) findViewById(R.id.imageButton0);
+        chef = (ImageButton) findViewById(R.id.imageButton3);
+        dessert = (ImageButton) findViewById(R.id.imageButton1);
+
+        Intent intentSettings = new Intent(this, my_reviews.class);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(intentSettings);
+            }
+        });
+
+        Intent nourished = new Intent(this, CurrentReviews.class);
+        nourish.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(nourished);
+            }
+        });
+
+        Intent grilled = new Intent(this, CurrentReviews.class);
+        grill.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(grilled);
+            }
+        });
+
+        Intent chefed = new Intent(this, CurrentReviews.class);
+        chef.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(chefed);
+            }
+        });
+
+        Intent desserted = new Intent(this, CurrentReviews.class);
+        dessert.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(desserted);
+            }
+        });
     }
 }
