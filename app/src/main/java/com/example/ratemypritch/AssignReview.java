2fe36f68eb6d4cb36e90891a7e6965607404a720
haniya1305 <haniya1305@gmail.com>
2022-12-05 17:10:58 -0800

    adding spellcheck functionailites

--- app/src/main/java/com/example/ratemypritch/AssignReview.java
+++ app/src/main/java/com/example/ratemypritch/AssignReview.java
@@ -10,6 +10,16 @@ import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class AssignReview extends AppCompatActivity {
 
     DB_Helper DB;
@@ -25,12 +35,14 @@ public class AssignReview extends AppCompatActivity {
         ImageButton assignreview = (ImageButton) findViewById(R.id.imageButton);
         DB = new DB_Helper(this);
 
+
         Intent intent = new Intent(this, CurrentReviews.class);
         backbutton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                startActivity(intent);
+                finish();
+//                startActivity(intent);
             }
         });
         Intent intent2 = new Intent(this, CurrentReviews.class);
@@ -39,6 +51,13 @@ public class AssignReview extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
+
+                try {
+                    spellchecker();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 if(assignedrating.getRating() == 0)
                 {
                     Toast.makeText(getApplicationContext(), "No Rating Inputted", Toast.LENGTH_SHORT).show();
@@ -57,13 +76,45 @@ public class AssignReview extends AppCompatActivity {
                     startActivity(intent2);
                 }
 
-
             }
         });
-
-
-
-
-
     }
+
+    void spellchecker() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://jspell-checker.p.rapidapi.com/check")
+                .get()
+                .addHeader("X-RapidAPI-Key", "b9dda96a07mshb89e7d029803e5dp1d7b7ejsnb0fa37653bc4")
+                .addHeader("X-RapidAPI-Host", "jspell-checker.p.rapidapi.com")
+                .build();
+
+
+        Response response = client.newCall(request).execute();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                AssignReview.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "There exists " + myResponse +" grammatical errors!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        });
+    }
+
+}
+
 }
